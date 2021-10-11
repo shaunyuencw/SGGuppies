3366f30b014fedd2e8723e8184b08b03a7792dbc
Sugiantoxyk <sugianto228@gmail.com>
2021-10-11 21:20:10 +0800

    change tabs

--- app/src/main/java/com/example/guppyhelp/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/guppyhelp/ui/main/SectionsPagerAdapter.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.guppyhelp.MapsFragment;
 import com.example.guppyhelp.R;
+import com.example.guppyhelp.RequestFragment;
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -27,9 +29,16 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new MapsFragment();
+                break;
+            case 1:
+                fragment = new RequestFragment();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
