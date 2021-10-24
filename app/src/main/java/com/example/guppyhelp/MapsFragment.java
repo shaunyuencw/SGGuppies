ca3700143b355b608d1f3dedc88a641216243bb1
Sugiantoxyk <sugianto228@gmail.com>
2021-10-24 17:20:17 +0800

    aed icon added, myLocationButton position change, map zoom

--- app/src/main/java/com/example/guppyhelp/MapsFragment.java
+++ app/src/main/java/com/example/guppyhelp/MapsFragment.java
@@ -8,19 +8,23 @@ import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -31,6 +35,7 @@ import java.util.List;
 
 public class MapsFragment extends Fragment {
     public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    Marker lastAccessedMarker = null;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -45,31 +50,50 @@ public class MapsFragment extends Fragment {
          */
 
         @Override
-        public void onMapReady(GoogleMap googleMap) {
+        public void onMapReady(final GoogleMap googleMap) {
 
             /** LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-             */
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(test, 15.0f));
+            */
 
-            // Test Marker (will be CHANGED after connected to SQL)
-            googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(1.372072, 103.848963))
-                    .title("Test Marker"));
-            googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(1.372136, 103.847054))
-                    .title("Test Marker"));
-            // googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(test, 15.0f));
             googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
                 @Override
                 public boolean onMarkerClick(@NonNull Marker marker) {
-                    // Change this to show AED detail
-                    Snackbar.make(getView(), "test", Snackbar.LENGTH_SHORT).show();
+                    closeAEDDetailPanel(lastAccessedMarker);
+                    showAEDDetails(googleMap, marker);
                     return true;
                 }
             });
 
-            // Check if permission granted, enable setMyLocationButton
+            googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener(){
+                @Override
+                public boolean onMyLocationButtonClick()
+                {
+                    closeAEDDetailPanel(lastAccessedMarker);
+                    updateSurroundingAED(googleMap);
+                    return false;
+                }
+            });
+
+            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(@NonNull LatLng latLng) {
+                    closeAEDDetailPanel(lastAccessedMarker);
+                }
+            });
+
+            // Get myLocationButton
+            SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+            final View myLocationButton = mapFragment.getView().findViewById(0x2);
+            // Change myLocationButton position
+            RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) myLocationButton.getLayoutParams();
+            rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP,0);
+            rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,RelativeLayout.TRUE);
+            rlp.setMargins(0,0,30,30);
+
+            // Check if permission granted, enable setMyLocation & setMyLocationButton
             if(checkAndRequestPermissions()) {
                 if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) ==
                         PackageManager.PERMISSION_GRANTED &&
@@ -82,8 +106,52 @@ public class MapsFragment extends Fragment {
                 }
             }
 
+            // When map is loaded, camera to current location
+            googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+                @Override
+                public void onMapLoaded() {
+                    myLocationButton.performClick();
+                }
+            });
         }
     };
+
+    private void showAEDDetails(GoogleMap googleMap, Marker marker){
+        // Show AED details
+        Snackbar.make(getView(), "AED CLICKED", Snackbar.LENGTH_SHORT).show();
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 16.0f));
+        marker.setIcon(bitmapDescriptorFromVector(getContext(), R.drawable.ic_aed_icon_2));
+        lastAccessedMarker = marker;
+    }
+
+    private void closeAEDDetailPanel(Marker marker){
+        if (marker != null){
+            marker.setIcon(bitmapDescriptorFromVector(getContext(), R.drawable.ic_aed_icon));
+        }
+    }
+
+    private void updateSurroundingAED(GoogleMap googleMap){
+        // SQL query to get all surrounding AED
+        Snackbar.make(getView(), "UPDATE SURROUND AED", Snackbar.LENGTH_SHORT).show();
+        googleMap.clear();
+        lastAccessedMarker = null;
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(1.372072, 103.848963))
+                .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_aed_icon)));
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(1.372136, 103.847054))
+                .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_aed_icon)));
+    }
+
+    // Change vector to bitmap
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
 
     private  boolean checkAndRequestPermissions() {
         int locationPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
